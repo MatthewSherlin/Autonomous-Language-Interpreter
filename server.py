54cf2e7e2f48ad2d16da50a8030412c83e4f8c74
IAmDylanDennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-10-10 19:49:13 -0400

    adding static file and route

--- server.py
+++ server.py
@@ -2,12 +2,23 @@ from bottle import route, get, post
 from bottle import run, debug
 from bottle import request, response, redirect, template
 from bottle import default_app
+from bottle import static_file
 
 @route("/")
 def loginPage():
     return template("login")
 
 
+
+#for images on page
+@route("/static/png/<filename:re:.*\.png>")
+@route("/image/<filename:re:.*\.png>")
+def get_image(filename):
+    return static_file(filename=filename, root="static/images", mimetype="image/png")
+
+@route("/static/<filename:path>")
+def get_static(filename):
+    return static_file(filename=filename, root="static")
 
 if __name__ == "__main__":
     debug(True)
