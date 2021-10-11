6452d2f8d0c55ce0382769fa038f6d76b0e7d3ae
IAmDylanDennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-10-10 21:02:38 -0400

    adding session, json and encryption functions into server

--- server.py
+++ server.py
@@ -3,10 +3,139 @@ from bottle import run, debug
 from bottle import request, response, redirect, template
 from bottle import default_app
 from bottle import static_file
+import json
+import random
+import string
+import hashlib
+import os
+import codecs
+
 
 @route("/")
 def loginPage():
     return template("login")
+
+
+
+
+
+
+
+
+
+
+#---------------session functions------------------------
+def getSession(request):
+    
+    def newSession(): #creates new session dic
+        sessionId = newSessionId()
+        s = { #creating dic
+            "session_id" : sessionId,
+            "username" : ''
+        }
+        return s #returning data
+
+    sessionId = request.get_cookie("session_id", default=None) #asking for browser given data
+    if sessionId == None:
+        s = newSession() #if none found create new
+    else: # if found, get it 
+        try:
+            s= read(sessionId) 
+        except: # exception for proctection
+            s = newSession()
+    return s #return session 
+
+#saving session 
+def saveSession(response, session):
+    write(session['session_id'], session) #write through json
+    response.set_cookie("session_id", session['session_id'], path="/") # sets session 
+
+#uses token function to get new session ID
+def newSessionId():
+    return createToken()
+
+#create token for session ID
+def createToken(k=32):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=k)) #creates random string
+
+
+
+
+
+#---------------------json file functions---------------------------------------
+def write(key, data):
+    assert type(data) is dict
+    with open(f"data/session.{key}.json", "w") as f: #writes to json file
+        json.dump(data,f) #dumb for write
+    return
+
+def read(key):
+    with open(f"data/session.{key}.json", "r") as f: #reads from json file
+        data = json.load(f) #load for read
+    assert type(data) is dict #make sure it is a dict data type
+    return data
+
+def getUser(name):
+    try:
+        with open(f"data/user.{name}.json", "r") as f: #reads from json file to find username
+            data = json.load(f) #load for read
+        assert type(data) is dict #makes sure is dic data ype
+        return data
+    except:
+        return None
+
+def saveUser(name, data):
+    assert type(data) is dict #make sure it is a dict data type
+    with open(f"data/user.{name}.json", "w") as f: #writes data to json file 
+        json.dump(data,f) #dump for write
+    return
+
+
+
+
+
+
+
+#------------------------Credential functions---------------------
+#function for hashing process
+def bytesToString(byte):
+    string = str(codecs.encode(byte,"hex"),"utf-8") #using utf-8 to change bytes to a string
+    assert type(string) is str #making sure its a string
+    return string
+#function for hashing process
+def stringToBytes(string):
+    byte = codecs.decode(bytes(string,"utf-8"),"hex") #changing from string to bytes
+    assert type(byte) is bytes #making sure its bytes
+    return byte
+
+#The hashing function 
+def generateCredentials(Userpassword):
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac(
+        'sha256', # The hash digest algorithm for HMAC
+        Userpassword.encode('utf-8'), #Makes password byes
+        salt, # Provide the salt
+        100000 # It is recommended to use at least 100,000 iterations of SHA-256 
+        )
+    return {  #returns hash
+        'salt':bytesToString(salt), 
+        'key' :bytesToString(key),
+    }
+
+#passwords need to be verified. We need to hash and compare to see if its verifiable 
+def verifyPassword(Userpassword, Usercredentials):
+    salt = stringToBytes(Usercredentials['salt']) #get salt
+    key  = stringToBytes(Usercredentials['key'])  #get key
+    
+    newKey = hashlib.pbkdf2_hmac( #process to hash the password to compare
+        'sha256', # The hash digest algorithm for HMAC
+        Userpassword.encode('utf-8'), # Convert the password to bytes
+        salt, # Provide the salt
+        100000 # It is recommended to use at least 100,000 iterations of SHA-256 
+        )
+    return newKey == key #returns bool to see if they match
+
+
 
 
 
