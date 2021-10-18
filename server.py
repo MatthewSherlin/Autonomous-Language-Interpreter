37983446824c242b276295a9020e353357ba44c3
IAmDylanDennison <71900262+IAmDylanDennison@users.noreply.github.com>
2021-10-18 18:22:42 -0400

    Added the log out function and route to the backend

--- server.py
+++ server.py
@@ -5,20 +5,20 @@ from bottle import request, response, redirect, template
 from bottle import default_app
 from bottle import static_file
 
-from sessions import saveSession, getSession, newSession
+from sessions import saveSession, getSession
 from database import companies, saveUser, getUser
-import json
-# import random
-import string
+
 import hashlib
 import os
 import codecs
 
+#----------------home page--------------------
+@route("/home")
+def homePage():
+    return template("home")
 
 
-
-#!!! please have username as 'username' inside mySql datbase. it will be easier in the future
-#login page functionality
+#-------------------login page functionality--------------------
 @get("/")
 def getloginPage():
     return template("login")
@@ -47,13 +47,8 @@ def postloginPage():
     return redirect('/home') #will redirect to home page with the user being logged in 
 
 
-#ADMIN create account functionality
-#code for create an account will be added here
 
-@route("/home")
-def homePage():
-    return template("home")
-
+#---------------sign up functionality ----------------
 @route("/signup")
 def signUpPage():
     return template("signup")
@@ -92,36 +87,13 @@ def postSignUp():
     return redirect('/')
 
 
-
-#---------------------json file functions---------------------------------------
-def write(key, data):
-    assert type(data) is dict
-    with open(f"data/session.{key}.json", "w") as f: #writes to json file
-        json.dump(data,f) #dumb for write
-    return
-
-def read(key):
-    with open(f"data/session.{key}.json", "r") as f: #reads from json file
-        data = json.load(f) #load for read
-    assert type(data) is dict #make sure it is a dict data type
-    return data
-
-""" def getUser(name):
-    try:
-        with open(f"data/user.{name}.json", "r") as f: #reads from json file to find username
-            data = json.load(f) #load for read
-        assert type(data) is dict #makes sure is dic data ype
-        return data
-    except:
-        return None
- """
-""" def saveUser(name, data):
-    assert type(data) is dict #make sure it is a dict data type
-    with open(f"data/user.{name}.json", "w") as f: #writes data to json file 
-        json.dump(data,f) #dump for write
-    return
- """
-
+#--------------sign out function & route-----------------------
+@get("/logout")
+def getLogout():
+    session = getSession(request) #get session
+    session['user_id'] = '' #change session info to no user_id
+    saveSession(response, session) #save session
+    return redirect('/') #redirect to login page
 
 
 
@@ -165,8 +137,6 @@ def verifyPassword(Userpassword, Usercredentials):
         100000 # It is recommended to use at least 100,000 iterations of SHA-256
         )
     return newKey == key #returns bool to see if they match
-
-
 
 
 
