51c93f31aa606393476914e1fca72fa0b321df8e
Jarrett Woo <jwoo1@kent.edu>
2021-11-01 20:23:49 -0400

    Switches implementation to Flask. Breaks sessions

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-#ALI server file
-from bottle import route, get, post 
-from bottle import run, debug
-from bottle import request, response, redirect, template
-from bottle import default_app
-from bottle import static_file
+# ALI server file
+##flask imports
+from flask import Flask, render_template
+from flask import request
+from flask import redirect, url_for
+
+# from bottle import route, get, post
+# from bottle import run, debug
+# from bottle import request, response, redirect, template
+# from bottle import default_app
+# from bottle import static_file
 
 from sessions import saveSession, getSession
 from database import companies, saveUser, getUser
@@ -12,149 +17,158 @@ import hashlib
 import os
 import codecs
 
-#----------------home page--------------------
-@route("/home")
+app = Flask(__name__)
+
+# ----------------home page--------------------
+@app.route("/home")
 def homePage():
-    return template("home")
+    return render_template("home.html")
 
 
-#-------------------login page functionality--------------------
-@get("/")
-def getloginPage():
-    return template("login", failedLogin = False)
+# -------------------login page functionality--------------------
+@app.route("/", methods=["GET", "POST"])
+def loginPage():
+    if request.method == "POST":
+        session = getSession(request)
+        # this will grab user input from html page
+        username = request.form["username"]
+        password = request.form["password"]
 
-@post("/")
-def postloginPage():
-    session = getSession(request)
-    # this will grab user input from html page
-    username = request.forms.get('username') 
-    password = request.forms.get('password')
+        user = getUser(username)
+        # print(type(user))  Debug prints
+        # print(f"username is type: {type(username)}")
+        # print(user)
+        #!!we need to add a pop up if user credentials is wrong. Because we can not redirect to signup page
+        # bootstrap has some cool alert messages
+        if not user:
+            return render_template(
+                "login.html", failedLogin=True
+            )  # if not found will redirect user back to login page
+        if "password" not in user:
+            return render_template("login.html", failedLogin=True)
+        if not verifyPassword(password, user["password"]):
+            return render_template(
+                "login.html", failedLogin=True
+            )  # if password is wrong will redirect to login page
+        session["user_id"] = username  # gives user name to session
+        saveSession(request.response, session)  # saves the session
+        return redirect(
+            url_for("home")
+        )  # will redirect to home page with the user being logged in
 
-    user = getUser(username)
-    print(type(user))
-    print(f"username is type: {type(username)}")
-    print(user)
-    #!!we need to add a pop up if user credentials is wrong. Because we can not redirect to signup page
-    #bootstrap has some cool alert messages 
-    if not user:
-        return template('login', failedLogin = True) #if not found will redirect user back to login page
-    if 'password' not in user:
-        return template('login', failedLogin = True)
-    if not verifyPassword(password, user['password']):
-        return template('login', failedLogin = True) #if password is wrong will redirect to login page
-    session['user_id'] = username #gives user name to session
-    saveSession(response, session) #saves the session
-    return redirect('/home') #will redirect to home page with the user being logged in 
+    else:
+        return render_template("login.html", failedLogin=False)
 
-   
 
-#---------------sign up functionality ----------------
-@route("/signup")
+# ---------------sign up functionality ----------------
+@app.route("/signup", methods=["GET", "POST"])
 def signUpPage():
-    return template("signup", invalidCode = False, notPasswordMatch = False)
+    if request.method == "POST":
+        session = getSession(request)  # get session
+        companyKey = request.form["companyKey"]
+        username = request.form["username"]  # get username form page
+        print("Getting password")
+        password = request.form["password"]  # get password from page
+        print(f"password is: {password}")
+        passwordRepeat = request.form["password_again"]  # get password from page
 
-@post("/signup")
-def postSignUp():
-    session = getSession(request) #get session 
-    companyKey = request.forms.get('companyKey')
-    username = request.forms.get('username') #get username form page
-    print("Getting password")
-    password = request.forms.get('password') #get password from page
-    print(f"password is: {password}")
-    passwordRepeat = request.forms.get('password_again') #get password from page
+        if (
+            password != passwordRepeat
+        ):  # makes sure the double password input is the same
+            saveSession(request.response, session)
+            return render_template(
+                "signup.html", invalidCode=False, notPasswordMatch=True
+            )  # will redirct to home page if not the same
 
-    if password != passwordRepeat: #makes sure the double password input is the same
-        saveSession(response, session) 
-        return template('signup', invalidCode = False, notPasswordMatch = True)    #will redirct to home page if not the same
-    
-    companyInfo = list(companies.find(company_key = companyKey))
-    try: 
-        companyName = companyInfo[0].get('company_name')  
-    except: 
-        # need to return error code rather than redirect
-        return template('signup', invalidCode = True, notPasswordMatch = False) #input message (bootstrap alert) that says company key wrong
-        
-    data = { #saves user after signup
-        'username': username,
-        'password': generateCredentials(password),
-        'company_name': companyName, #change to company name
-        'user_id' : username
-    }
-    print(type(data))
-    saveUser(data)
-    session['user_id'] = username #sets session user name to the new users name
-    saveSession(response, session)
-    return redirect('/')
+        companyInfo = list(companies.find(company_key=companyKey))
+        try:
+            companyName = companyInfo[0].get("company_name")
+        except:
+            # need to return error code rather than redirect
+            return render_template(
+                "signup.html", invalidCode=True, notPasswordMatch=False
+            )  # input message (bootstrap alert) that says company key wrong
+
+        data = {  # saves user after signup
+            "username": username,
+            "password": generateCredentials(password),
+            "company_name": companyName,  # change to company name
+            "user_id": username,
+        }
+        print(type(data))
+        saveUser(data)
+        session["user_id"] = username  # sets session user name to the new users name
+        saveSession(request.response, session)
+        return redirect(url_for("login"))
+    else:
+        return render_template("signup.html", invalidCode=False, notPasswordMatch=False)
 
 
-#--------------sign out function & route-----------------------
-@get("/logout")
+# --------------sign out function & route-----------------------
+@app.route("/logout", methods=["GET"])
 def getLogout():
-    session = getSession(request) #get session
-    session['user_id'] = '' #change session info to no user_id
-    saveSession(response, session) #save session
-    return redirect('/') #redirect to login page
+    session = getSession(request)  # get session
+    session["user_id"] = ""  # change session info to no user_id
+    saveSession(request.response, session)  # save session
+    return redirect(url_for("login"))  # redirect to login page
 
 
-
-
-
-#------------------------Credential functions---------------------
-#function for hashing process
+# ------------------------Credential functions---------------------
+# function for hashing process
 def bytesToString(byte):
-    string = str(codecs.encode(byte,"hex"),"utf-8") #using utf-8 to change bytes to a string
-    assert type(string) is str #making sure its a string
+    string = str(
+        codecs.encode(byte, "hex"), "utf-8"
+    )  # using utf-8 to change bytes to a string
+    assert type(string) is str  # making sure its a string
     return string
-#function for hashing process
+
+
+# function for hashing process
 def stringToBytes(string):
-    byte = codecs.decode(bytes(string,"utf-8"),"hex") #changing from string to bytes
-    assert type(byte) is bytes #making sure its bytes
+    byte = codecs.decode(bytes(string, "utf-8"), "hex")  # changing from string to bytes
+    assert type(byte) is bytes  # making sure its bytes
     return byte
 
-#The hashing function 
+
+# The hashing function
 def generateCredentials(Userpassword):
     salt = os.urandom(32)
     key = hashlib.pbkdf2_hmac(
-        'sha256', # The hash digest algorithm for HMAC
-        Userpassword.encode('utf-8'), #Makes password byes
-        salt, # Provide the salt
-        100000 # It is recommended to use at least 100,000 iterations of SHA-256 
-        )
-    return {  #returns hash
-        'salt':bytesToString(salt), 
-        'key' :bytesToString(key),
+        "sha256",  # The hash digest algorithm for HMAC
+        Userpassword.encode("utf-8"),  # Makes password byes
+        salt,  # Provide the salt
+        100000,  # It is recommended to use at least 100,000 iterations of SHA-256
+    )
+    return {  # returns hash
+        "salt": bytesToString(salt),
+        "key": bytesToString(key),
     }
 
-#passwords need to be verified. We need to hash and compare to see if its verifiable 
+
+# passwords need to be verified. We need to hash and compare to see if its verifiable
 def verifyPassword(Userpassword, Usercredentials):
-    salt = stringToBytes(Usercredentials['salt']) #get salt
-    key  = stringToBytes(Usercredentials['key'])  #get key
-    
-    newKey = hashlib.pbkdf2_hmac( #process to hash the password to compare
-        'sha256', # The hash digest algorithm for HMAC
-        Userpassword.encode('utf-8'), # Convert the password to bytes
-        salt, # Provide the salt
-        100000 # It is recommended to use at least 100,000 iterations of SHA-256
-        )
-    return newKey == key #returns bool to see if they match
+    salt = stringToBytes(Usercredentials["salt"])  # get salt
+    key = stringToBytes(Usercredentials["key"])  # get key
+
+    newKey = hashlib.pbkdf2_hmac(  # process to hash the password to compare
+        "sha256",  # The hash digest algorithm for HMAC
+        Userpassword.encode("utf-8"),  # Convert the password to bytes
+        salt,  # Provide the salt
+        100000,  # It is recommended to use at least 100,000 iterations of SHA-256
+    )
+    return newKey == key  # returns bool to see if they match
 
 
+# for images on page
+# @app.route("/static/png/<filename:re:.*\.png>")
+# @app.route("/image/<filename:re:.*\.png>")
+# def get_image(filename):
+#     return app.send_static_file(filename=filename, root="static/images", mimetype="image/png")
 
-#for images on page
-@route("/static/png/<filename:re:.*\.png>")
-@route("/image/<filename:re:.*\.png>")
-def get_image(filename):
-    return static_file(filename=filename, root="static/images", mimetype="image/png")
-
-@route("/static/<filename:path>")
-def get_static(filename):
-    return static_file(filename=filename, root="static")
-
-
+# @app.route("/static/<filename:path>")
+# def get_static(filename):
+#     return app.send_static_file(filename=filename, root="static")
 
 
 if __name__ == "__main__":
-    debug(True)
-    run(host="localhost", port=8080)
-else:
-    application = default_app()
+    app.run(host="localhost", port=8080, debug=True)
--- sessions.py
+++ sessions.py
@@ -1,13 +1,12 @@
 # sessions
 from database import db, session_table
-
+from flask import make_response
 import mysql.connector
 import os
 import string
 import random
 import json
-
-
+from flask import Flask, render_template_string, request, session, redirect, url_for
 
 # ---------------session functions------------------------
 def newSession():  # creates new session dic
@@ -17,7 +16,7 @@ def newSession():  # creates new session dic
 
 
 def getSession(request):
-    sessionId = request.get_cookie(
+    sessionId = request.cookies.get(
         "session_id", default=None
     )  # asking for browser given data
     if sessionId == None:
@@ -26,11 +25,13 @@ def getSession(request):
         try:
             # s = read(sessionId)
             session = list(session_table.find(session_id=sessionId))
-            sessionId = session[0].get('session_id')
-            s = {'session_id': sessionId, 'user_id': ""}
+            sessionId = session[0].get("session_id")
+            s = {"session_id": sessionId, "user_id": ""}
         except:  # exception for proctection
             s = newSession()
     return s  # return session
+   # current_session = session['username']
+    #return current_session
 
 
 # saving session
@@ -38,21 +39,29 @@ def saveSession(response, session):
     assert type(session) is dict
 
     # updates the session with each save
-    data = {'session_id':  session['session_id'], 'user_id':session['user_id']}
-   
-    results = list(session_table.find(session_id=session['session_id']))
-    
+    data = {"session_id": session["session_id"], "user_id": session["user_id"]}
+
+    results = list(session_table.find(session_id=session["session_id"]))
+
     # if nothing is found a new session is inserted
     if results == []:
-        
-        session_table.insert({'session_id': session['session_id'], 'user_id':session['user_id']})
-        response.set_cookie('session_id', session['session_id'], path="/")
 
-    #else the session is updated
+        session_table.insert(
+            {"session_id": session["session_id"], "user_id": session["user_id"]}
+        )
+        response.set_cookie("session_id", session["session_id"], path="/")
+
+    # else the session is updated
     else:
-         session_table.update(data, ['session_id'])
-         response.set_cookie('session_id', session['session_id'], path="/")
-    
+        session_table.update(data, ["session_id"])
+        response.set_cookie("session_id", session["session_id"], path="/")
+
+
+# @app.route('/')
+# def index():
+#     resp = make_response(render_template(...))
+#     resp.set_cookie('username', 'the username')
+#     return resp
 
 
 # uses token function to get new session ID
@@ -65,4 +74,3 @@ def createToken(k=32):
     return "".join(
         random.choices(string.ascii_lowercase + string.digits, k=k)
     )  # creates random string
-
