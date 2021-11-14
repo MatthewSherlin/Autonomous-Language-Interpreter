c8079e14a14d21bac0d7310a7d21ac097074bec2
Daniel Hinz <dhinz1@kent.edu>
2021-11-14 15:51:32 -0500

    pulled database.py from main

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
-# database
 import dataset
 
+
+import hashlib
+import os
+import codecs
+
+import string
+import random
 ## connecting to database
 ## !!!may need to put root as password if no connection occurs!!!
 ## make sure database = ali ##
@@ -13,15 +19,19 @@ db = dataset.connect("mysql://root:@localhost/ali")
 users_table = db["users_table"]
 companies = db["companies"]
 chart_table = db["chart_table"]
+sessions = db["sessions"]
 
+## 
+def companyIdGenerator(size=4, uchars=string.digits):
+    return ''.join(random.choice(uchars) for _ in range(size))
 
 # Testing database setup
 db["companies"].drop()
 
 companies.insert(
     {
-        "company_id": "1234",
-        "company_name": "Avita",
+        "company_id": "0001",
+        "company_name": "ALI",
         "company_key": "123thjmv79cdfj3ki5tye",
     }
 )
@@ -52,6 +62,7 @@ def getUser(username):
         username = user[0].get("username")
         password = user[0].get("password")
         company = user[0].get("company_name")
+        isAdmin = user[0].get("is_admin")
         data = {"username": username, "password": password, "company_name": company}
         return data
     except:
@@ -65,7 +76,60 @@ def saveUser(data):
         {
             "username": data["username"],
             "password": data["password"],
-            "company_name": data["company_name"],
+            "company_name": data["company_name"]
         }
     )
     return
+
+def saveCompany(data):
+    assert type(data) is dict
+    #try:
+    #companies.find(company_key = data["company_key"])
+    #except:
+    companies.insert(
+        {
+            "company_id": data["company_id"],
+            "company_name": data["company_name"],
+            "company_key": data["company_key"]
+        }
+    )
+    return
+
+def isAdmin(username):
+    if username  == "admin":
+        return True
+    else: 
+        return False
+
+
+
+# ------------------------Credential functions---------------------
+# function for hashing process
+def bytesToString(byte):
+    string = str(
+        codecs.encode(byte, "hex"), "utf-8"
+    )  # using utf-8 to change bytes to a string
+    assert type(string) is str  # making sure its a string
+    return string
+
+
+# function for hashing process
+def stringToBytes(string):
+    byte = codecs.decode(bytes(string, "utf-8"), "hex")  # changing from string to bytes
+    assert type(byte) is bytes  # making sure its bytes
+    return byte
+
+
+# The hashing function
+def generateCredentials(Userpassword):
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac(
+        "sha256",  # The hash digest algorithm for HMAC
+        Userpassword.encode("utf-8"),  # Makes password byes
+        salt,  # Provide the salt
+        100000,  # It is recommended to use at least 100,000 iterations of SHA-256
+    )
+    return {  # returns hash
+        "salt": bytesToString(salt),
+        "key": bytesToString(key),
+    }
