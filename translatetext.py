0a4e3f6f3e779df9cfe4dd682b1c2ce22222c082
Sam Hill <72173348+SamH7798@users.noreply.github.com>
2021-11-19 20:08:15 -0500

    fixing homepage bug

--- translatetext.py
+++ translatetext.py
@@ -25,7 +25,7 @@ def takeHomeTranslate(var1, text):
         file.write(savechanges)
         file.close()
 
-
+##clears textarea in takehome.html page
 def clearTextTags(): 
     with open('templates/takeHome.html', 'rb') as file:
         soup = BeautifulSoup(file.read(), "lxml") 
@@ -38,3 +38,15 @@ def clearTextTags():
         file.write(savechanges)
         file.close()
 
+#clears text area in home.html
+def clearHomeTags(): 
+    with open('templates/home.html', 'rb') as file:
+        soup = BeautifulSoup(file.read(), "lxml") 
+        soup.find("textarea", {"id": "t1"}).clear()
+        soup.find("textarea", {"id": "t2"}).clear()
+        file.close()
+
+    savechanges = soup.prettify("utf-8")
+    with open("templates/home.html", "wb") as file:
+        file.write(savechanges)
+        file.close()
